10affa0741291aee339fe5204b7c66a1b3f976e5
hal9000 <hal9000@localhost.localdomain>
2023-10-03 15:06:54 +0200

    experiment 2

--- experiments/gzip_experiments/0x1.py
+++ experiments/gzip_experiments/0x1.py
@@ -1,9 +1,37 @@
+import gzip 
+
 """
 subset pruning experiment
 """
 
+def jointy_compress(*elem:str) -> bytes:
+    """
+    takes in N strings concatenates them together and 
+    compresses them with gzip
+    """
+    string = " ".join(elem) #join with space seperator
+    return gzip.compress(bytes(string,encoding="utf-8"))
+    
+
 A = "test"
-B = "test test test"
+B = "test test"
+C =  "test test test"
 
-C = "lol lol lol lol test test test lol lol lol "
+target = "lol lol lol lol test test test lol lol lol "
+print("word overlap matters?")
+print(len(jointy_compress(A,target))) 
+print(len(jointy_compress(B,target)))
+print(len(jointy_compress(C,target)))
 
+"""
+note that lower overlap can sometimes have a shorter length
+"""
+
+print("\nsymetry in the operation?")
+print("target+A: ",len(jointy_compress(target,A))," A+target:",len(jointy_compress(A,target))) 
+print("target+B: ",len(jointy_compress(target,B))," B+target:",len(jointy_compress(B,target)))
+print("target+C: ",len(jointy_compress(target,C))," c+target:",len(jointy_compress(C,target)))
+
+"""
+note that the operation is not symetrical
+"""
