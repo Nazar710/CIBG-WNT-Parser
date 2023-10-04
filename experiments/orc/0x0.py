ae48bc177f334f7b01578dceabcc46a708863366
zijiandongkurt <57815948+zijiandongkurt@users.noreply.github.com>
2023-10-04 17:36:03 +0200

    00

--- experiments/orc/0x0.py
+++ experiments/orc/0x0.py
@@ -1,5 +1,21 @@
 import easyocr
+from PIL import Image
+
 """
 https://github.com/JaidedAI/EasyOCR
 """
 
+import easyocr
+
+# Initialize the EasyOCR reader with the desired language(s)
+reader = easyocr.Reader(['en'])  # You can specify multiple languages if needed
+
+# Load an image for OCR
+image_path = '1.png'
+
+# Perform OCR on the image
+results = reader.readtext(image_path)
+
+# Print the OCR results
+for (bbox, text, prob) in results:
+    print(f'Text: {text}, Probability: {prob}')
