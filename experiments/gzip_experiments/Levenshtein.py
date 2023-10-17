a9a41de3654b843d939f3af48a3f7704a4b9c8ed
hal9000 <hal9000@localhost.localdomain>
2023-10-17 12:27:03 +0200

    rename

---
+++ experiments/gzip_experiments/Levenshtein.py
@@ -0,0 +1,33 @@
+import gzip 
+import Levenshtein
+
+def jointy_compress(*elem:str) -> bytes:
+    """
+    takes in N strings concatenates them together and 
+    compresses them with gzip
+    """
+    string = " ".join(elem) #join with space seperator
+    return gzip.compress(bytes(string,encoding="utf-8"))
+
+
+
+"""
+try using Levenshtein distance for pruning
+https://maxbachmann.github.io/Levenshtein/
+pip install levenshtein
+"""
+
+# A ="test test test"
+# B = "lol lol lol lol test test lol lol lol  test test"
+
+
+A ="test test hello"
+B = "lol lol lol lol test test lol lol lol"
+dist = Levenshtein.distance(A,B)
+
+mb = Levenshtein.matching_blocks(Levenshtein.editops(A, B),A,B)
+print(mb)
+
+for alignment in mb:
+    print(A[:alignment.a] +"["+ A[alignment.a:alignment.a+alignment.size] + "]" + A[alignment.a+alignment.size:])
+    print(B[:alignment.b] +"["+ B[alignment.b:alignment.b+alignment.size] + "]" + B[alignment.b+alignment.size:] +"\n")
