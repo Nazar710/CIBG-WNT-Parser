0804e2d367175fc18f1c89930dd313eab7e339b3
hal9000 <hal9000@localhost.localdomain>
2024-01-19 14:06:28 +0100

    lol

--- src_phase3/pipeline.py
+++ src_phase3/pipeline.py
@@ -40,72 +40,82 @@ class evaluator():
         """
         """
 
-        phonebook = pd.read_excel(self.a1tablePagePhoneBook_path)
-        phonebook = phonebook[phonebook["1a present"] == 1]
+        total_phonebook = pd.read_excel(self.a1tablePagePhoneBook_path)
+        A1_phonebook = total_phonebook[total_phonebook["1a present"] == 1]
 
-        for path_label,file_name in a1checkerMain.extractor.recursiveFilePathIterator(self.labelled_folder_path,["xlsx"]):
+        for path_label,labeled_file_name in a1checkerMain.extractor.recursiveFilePathIterator(self.labelled_folder_path,["xlsx"]):
+            pdf_file_name =labeled_file_name[:-5]+".pdf"
+
+            if( (A1_phonebook["File name"] ==pdf_file_name).sum() == 1):
+                print(total_phonebook[total_phonebook["File name"] == labeled_file_name])
+            else:
+                continue
+            
             #labeled file
             labeled_file = pd.read_excel(path_label)
 
+            exit()
             #output file 
             for output_file_name in os.listdir(self.output_folder_path):
-                if(len(file_name[:-4]) <= len(output_file_name)  and file_name[:-4] is output_file_name[:len(file_name[:-4])]):
-                    pass
+
+                print(output_file_name)    
+                # if(len(file_name[:-4]) <= len(output_file_name)  and file_name[:-4] is output_file_name[:len(file_name[:-4])]):
+                #     print(self.output_folder_path, " :  ", path_label)
 
 def pipeline(pdf_path_list:list[str], output_folder_path: str,hidden_progress_bar = True,evaluater:evaluator=None):
-    #hyper param
-    treshold = 0.7 
-    keywords = ["bezoldiging", "wnt"]
-    minNumRowsMatched = 10
-    tesseract_cmd_path = r'D:/CODING/Tesseract-OCR/tesseract.exe'
+    # #hyper param
+    # treshold = 0.7 
+    # keywords = ["bezoldiging", "wnt"]
+    # minNumRowsMatched = 10
+    # tesseract_cmd_path = r'D:/CODING/Tesseract-OCR/tesseract.exe'
     
         
-    Extractor = a1checkerMain.extractor()
-    checker = a1checkerMain.a1checker()
+    # Extractor = a1checkerMain.extractor()
+    # checker = a1checkerMain.a1checker()
 
-    wrappedPdfs =[checker.is1aOrNot(pdfobj,treshold,minNumRowsMatched) for pdfobj in Extractor.extractFromPathList(pdf_path_list)]
-    print("STTART!!!")
-    for wrappedPDF in tqdm(wrappedPdfs,ascii=True,hidden_progress_bar=hidden_progress_bar):
-        pdf_path = wrappedPDF.file_path
-        candidate_finder = speedy_candidates.candidates(keywords)
-        #candidate pages 
-        candidate_pages = candidate_finder.get_candidates(pdf_path,hide_progress_bar=hidden_progress_bar)
+    # wrappedPdfs =[checker.is1aOrNot(pdfobj,treshold,minNumRowsMatched) for pdfobj in Extractor.extractFromPathList(pdf_path_list)]
 
-        for pagenumber in candidate_pages:     
-            #exact match 
-            exact_match_table,isExactMatch = exactmatch_whitespace.extract_data_from_pdf(pdf_path,pagenumber,minNumRowsMatched)
+    # for wrappedPDF in wrappedPdfs:
+    #     pdf_path = wrappedPDF.file_path
+    #     candidate_finder = speedy_candidates.candidates(keywords)
+    #     #candidate pages 
+    #     candidate_pages = candidate_finder.get_candidates(pdf_path,hide_progress_bar=hidden_progress_bar)
+
+    #     for pagenumber in candidate_pages:     
+    #         #exact match 
+    #         exact_match_table,isExactMatch = exactmatch_whitespace.extract_data_from_pdf(pdf_path,pagenumber,minNumRowsMatched)
             
-            if(isExactMatch):
-                wrappedPDF.add_page(page_number=pagenumber,selectable=True,scanned=False,has_1a_table=True,csv_path="",csv_method="exact white space",tables=exact_match_table)
-            else: #not exact match 
-                whiteSpace(pdf_path,pagenumber,wrappedPDF)
+    #         if(isExactMatch):
+    #             wrappedPDF.add_page(page_number=pagenumber,selectable=True,scanned=False,has_1a_table=True,csv_path="",csv_method="exact white space",tables=exact_match_table)
+    #         else: #not exact match 
+    #             whiteSpace(pdf_path,pagenumber,wrappedPDF)
 
-        if(not wrappedPDF.has1ATable):
+    #     if(not wrappedPDF.has1ATable):
             
-            for pagenum in tqdm(candidate_finder.needsOCR(pdf_path,hidden_progress_bar),ascii=True,desc="OCR"):
-                # Create an instance of SearchablePDFConverter
-                pdf_converter = scannedConvert.SearchablePDFConverter(pdf_path, tesseract_cmd_path)
+    #         for pagenum in tqdm(candidate_finder.needsOCR(pdf_path,hidden_progress_bar),ascii=True,desc="OCR"):
+    #             # Create an instance of SearchablePDFConverter
+    #             pdf_converter = scannedConvert.SearchablePDFConverter(pdf_path, tesseract_cmd_path)
 
-                # Example 2: Convert a specific page of the PDF to a searchable PDF with post-processing
-                searchable_pdf_page = pdf_converter.convert_to_searchable_pdf_page(pagenum)
+    #             # Example 2: Convert a specific page of the PDF to a searchable PDF with post-processing
+    #             searchable_pdf_page = pdf_converter.convert_to_searchable_pdf_page(pagenum)
 
-                # Save the output searchable PDF for the specific page to a file
-                searchable_pdf_page_output_path = f'tempSearchable.pdf'
-                searchable_pdf_page.save(searchable_pdf_page_output_path)
-                whiteSpace("tempSearchable.pdf",pagenum,wrappedPDF)
+    #             # Save the output searchable PDF for the specific page to a file
+    #             searchable_pdf_page_output_path = f'tempSearchable.pdf'
+    #             searchable_pdf_page.save(searchable_pdf_page_output_path)
+    #             whiteSpace("tempSearchable.pdf",pagenum,wrappedPDF)
                 
-    for pdf in tqdm(wrappedPdfs):
-        for page in pdf.pages:
-            if page.has_1a_table:
-                if type(page._tables) is list:
-                    for tablenum, table in enumerate(page._tables):
-                        csv_path = os.path.join(output_folder_path, pdf.file_name[:-4]+str(page.page_number+tablenum)+".csv")
-                        table.to_csv(csv_path)
-                        page.csv_path = csv_path
-                else:
-                    csv_path = os.path.join(output_folder_path, str(pdf.file_name[:-4] + str(page.page_number)+".csv"))
-                    page.csv_path = csv_path
-                    page._tables.to_csv(csv_path)
+    # for pdf in tqdm(wrappedPdfs):
+    #     for page in pdf.pages:
+    #         if page.has_1a_table:
+    #             if type(page._tables) is list:
+    #                 for tablenum, table in enumerate(page._tables):
+    #                     csv_path = os.path.join(output_folder_path, pdf.file_name[:-4]+str(page.page_number+tablenum)+".csv")
+    #                     table.to_csv(csv_path)
+    #                     page.csv_path = csv_path
+    #             else:
+    #                 csv_path = os.path.join(output_folder_path, str(pdf.file_name[:-4] + str(page.page_number)+".csv"))
+    #                 page.csv_path = csv_path
+    #                 page._tables.to_csv(csv_path)
 
     if(evaluater is not None):
         subprocess.run(["clear"])
